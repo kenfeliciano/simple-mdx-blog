d05c8afd07cb8a80a3636ec412394ef5c52ffed0
Ken Feliciano <kafeliciano@gmail.com>
2020-09-05 16:00:48 -0700

    Add styled-components and initial theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
