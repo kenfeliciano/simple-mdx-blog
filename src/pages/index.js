485249cbbb3074db12250c0e0b2067c6bdb64f4b
Ken Feliciano <kafeliciano@gmail.com>
2020-09-06 14:58:44 -0700

    Create Content Card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Container, FeatureImage } from '../components'
+import { Container, FeatureImage, Content, ContentCard } from '../components'
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date='September 6, 2020'
+          title='Lorem ipsum baby!'
+          excerpt='Amet sit dolor ludus tonalis'
+          slug='/test'
+        />
+      </Content>
     </Container>
   )
 }
